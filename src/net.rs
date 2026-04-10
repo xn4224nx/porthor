ff0faea07894ee5d81f369719f88d7ed0ee0dffc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-04-10 04:24:48 +0100

    Random connection function.

--- src/net.rs
+++ src/net.rs
@@ -5,12 +5,51 @@
  * Functions to detect a sandbox by making network connections.
  */
 
+use rand::{RngExt, rngs::SmallRng};
+use std::net::TcpStream;
+
 /// Sandboxes will allow the executable to connect to any doamin and pretend
 /// that it was successful. This function connects to domains that do not exist.
 /// If these connections are successful the executable is most likely in a
 /// sandbox.
 pub fn connect_to_random_domain() -> Option<usize> {
-    None
+    let mut successful_connections = 0;
+    let letters = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    let top_level_domains = vec!["com", "org", "net", "gov", "edu"];
+
+    /* First check that there is network access by connecting to a dns server. */
+    TcpStream::connect("8.8.8.8:53").ok()?;
+
+    /* Generate and then try and connect to 10 domains. */
+    let mut rng: SmallRng = rand::make_rng();
+    for _ in 0..10 {
+        let site_len = rng.random_range(5..=12);
+
+        /* Generate the site name. */
+        let site = (0..site_len)
+            .map(|_| letters[rng.random_range(0..letters.len())])
+            .collect::<String>();
+
+        /* Create the full site url. */
+        let url = format!(
+            "http://www.{}.{}",
+            site,
+            top_level_domains[rng.random_range(0..top_level_domains.len())]
+        );
+
+        /* Attempt to connect to it. */
+        let Ok(res) = reqwest::blocking::get(url) else {
+            continue;
+        };
+
+        if res.status().is_success() {
+            successful_connections += 1;
+        };
+    }
+    return Some(successful_connections);
 }
 
 /// Sleep functions can be altered before inserting the program in a sandbox.
@@ -31,7 +70,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn run_iconnect_to_random_domain() {
+    fn run_connect_to_random_domain() {
         assert_eq!(connect_to_random_domain(), Some(0));
     }
 
