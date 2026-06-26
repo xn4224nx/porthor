407aa2674971022edcac2c773d45b0c90b8a8c1d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-06-26 17:43:35 +0100

    Added simple example.

---
+++ examples/simple.rs
@@ -0,0 +1,12 @@
+#![crate_type = "bin"]
+
+use porthor;
+
+fn main() {
+    println!("Network:           {:?}", porthor::is_network_valid());
+    println!("Uptime:            {:?}", porthor::is_uptime_valid());
+    println!("Temperature:       {:?}", porthor::is_temp_valid());
+    println!("System Attributes: {:?}", porthor::is_sys_attr_valid());
+    println!("Storage:           {:?}", porthor::is_storage_valid());
+    println!("Sleep:             {:?}", porthor::is_sleep_valid());
+}
