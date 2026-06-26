678e84aaf4612a401ddead4e8404ec2e5d1eb4a7
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-06-26 18:10:31 +0100

    Added further functions to the simple example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,10 +3,15 @@
 use porthor;
 
 fn main() {
+    println!(
+        "Domain Connection: {:?}",
+        porthor::connect_to_random_domain()
+    );
     println!("Network:           {:?}", porthor::is_network_valid());
     println!("Uptime:            {:?}", porthor::is_uptime_valid());
     println!("Temperature:       {:?}", porthor::is_temp_valid());
     println!("System Attributes: {:?}", porthor::is_sys_attr_valid());
     println!("Storage:           {:?}", porthor::is_storage_valid());
     println!("Sleep:             {:?}", porthor::is_sleep_valid());
+    println!("NTP Sleep:         {:?}", porthor::ntp_sleep_check());
 }
